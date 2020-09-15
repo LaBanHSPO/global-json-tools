423037bd2359b7acf17baa156ba4d9476fc9de4b
VTLOG <vtlog@gmail.com>
2020-09-15 16:48:55 +0700

    bannv

---
+++ README.md
@@ -0,0 +1,12 @@
+### JSON Tools for data manipulation
+
+### Install
+```
+    yarn global add jsonto
+```
+
+### Usage
+```
+    jsonto in.json result/out.xlsx
+```
+
---
+++ bin/index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const Excel = require('exceljs')
+
+try {
+    const inputFile = process.argv[2];
+    const outputPath = process.argv[3];
+    const existedFile =fs.existsSync(inputFile);
+    if (!existedFile) {
+        throw new Error('File is not exist')
+    }
+    console.log(`[JSON to XLSX] Reading files ${inputFile}....`);
+    const fileContent = fs.readFileSync(inputFile);
+
+    console.log('[JSON to XLSX] Parsed files...');
+    let parsed = JSON.parse(fileContent);
+    if (!Array.isArray(parsed)) throw new Error('Invalid input array');
+    if (Array.isArray(parsed[0])) {
+        parsed = parsed[0];
+    }
+
+    let workbook = new Excel.Workbook()
+
+    let worksheet = workbook.addWorksheet('excel_data')
+
+    console.log('COLUMNS: ', Object.keys(parsed[0]).map(i => ({ header: i, key: i })));
+
+    worksheet.columns = Object.keys(parsed[0]).map(i => ({ header: i, key: i }));
+
+    worksheet.columns.forEach(column => {
+        column.width = column.header.length < 12 ? 12 : column.header.length
+    });
+
+    worksheet.getRow(1).font = { bold: true }
+
+    // Dump all the data into Excel
+    parsed.forEach((e, index) => {
+        // row 1 is the header.
+        const rowIndex = index + 2
+
+        // By using destructuring we can easily dump all of the data into the row without doing much
+        // We can add formulas pretty easily by providing the formula property.
+        worksheet.addRow({
+            ...e
+        });
+    });
+    console.log('[JSON to XLSX] Writing file ...');
+    workbook.xlsx.writeFile(outputPath);
+    console.log('[JSON to XLSX] Done');
+
+} catch (err) {
+    console.log('[ERROR]', err.message);
+}
